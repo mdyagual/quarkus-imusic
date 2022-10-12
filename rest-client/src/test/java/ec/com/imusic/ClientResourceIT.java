b2bfed0028c5fee19588cf1a120edecdd8e72836
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-10-12 11:18:34 -0500

    Initial commit

---
+++ rest-client/src/test/java/ec/com/imusic/ClientResourceIT.java
@@ -0,0 +1,8 @@
+package ec.com.imusic;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ClientResourceIT extends ClientResourceTest {
+    // Execute the same tests but in packaged mode.
+}
